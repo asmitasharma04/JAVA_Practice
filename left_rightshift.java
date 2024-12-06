979877609ceff84556bcd1aa3183e42b07df501a
Asmita Sharma <155943217+asmitasharma04@users.noreply.github.com>
2024-12-06 19:09:18 +0530

    Add files via upload

---
+++ left_rightshift.java
@@ -0,0 +1,15 @@
+public class left_rightshift {
+    public static void main(String[] args) {
+        int g=20;
+        int f=g<<1;
+        System.out.println(f);
+        int a=10;
+        int b=30;
+        int c=a&b;
+        int d=a|b;
+        int e=a^b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
