7db1faa13452ce788fabfd17959f4c5187526794
Asmita Sharma <155943217+asmitasharma04@users.noreply.github.com>
2024-12-11 13:30:03 +0530

    Add files via upload

---
+++ reverse_swap.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class reverse_swap {
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5};
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            swap(arr,start,end);
+                start++;
+            
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public static void swap(int []arr,int index1,int index2) {
+        int temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+    }
+}
